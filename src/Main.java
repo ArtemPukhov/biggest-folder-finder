a6935a7c568299c735624108df08be6fe6034bae
artemp <artemm2006@list.ru>
2023-05-20 16:30:17 +0700

    Improved class ParametersBag

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,28 @@
 import java.io.File;
 import java.util.concurrent.ForkJoinPool;
 
-public class Main {
-
-    public static void main(String[] args) {
-
+public class Main
+{
+    public static void main(String[] args)
+    {
         ParametersBag bag = new ParametersBag(args);
 
-       String folderPath = bag.getPath();
-       long sizeLimit = bag.getLimit();
+        String folderPath = bag.getPath();
+        long sizeLimit = bag.getLimit();
 
         File file = new File(folderPath);
         Node root = new Node(file, sizeLimit);
 
         long start = System.currentTimeMillis();
 
-        FolderSizeCalculator calculator = new FolderSizeCalculator(root);
+        FolderSizeCalculator calculator =
+                new FolderSizeCalculator(root);
         ForkJoinPool pool = new ForkJoinPool();
         pool.invoke(calculator);
+
         System.out.println(root);
 
         long duration = System.currentTimeMillis() - start;
-        System.out.println(duration + "ms");
+        System.out.println(duration + " ms");
     }
 }
